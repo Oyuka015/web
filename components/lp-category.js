547260698c8b6ee8fa00195a6ba97374fda8a419
oyu43500 <oyu43500@gmail.com>
2025-12-15 00:00:20 +0800

    s

--- components/lp-category.js
+++ components/lp-category.js
@@ -13,14 +13,14 @@ class LpCategory extends HTMLElement {
                 .selectedCat{
                     font-size: 20px; 
                     background: var(--color-orange-lighter); 
-                    padding: 15px 30px; 
+                    padding: 8px 30px; 
                     border-radius: 10px; 
                     color: var(--color-white-0);
                 }
                 .defCat{
                     font-size: 20px; 
                     background: var(--color-white-1); 
-                    padding: 15px 30px; 
+                    padding: 8px 30px; 
                     border-radius: 10px; 
                     color: var(--text-color-default);
                 }
